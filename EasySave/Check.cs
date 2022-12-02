20dcba4b76df63ef8c1e1a1611f579ad9092e3ca
BaditSad <bdumortier0@gmail.com>
2022-12-02 10:16:19 +0100

    Update check folders and files

--- EasySave/Check.cs
+++ EasySave/Check.cs
@@ -11,48 +11,84 @@ namespace EasySave
     {
         public void ChekFolder()
         {
+            //Create folders
             Values.Instance.PathConfig = Directory.GetCurrentDirectory();
             if (!Directory.Exists(Values.Instance.PathConfig + "\\Config"))
             {
                 Directory.CreateDirectory(Values.Instance.PathConfig + "\\Config");
                 Values.Instance.PathTarget = Values.Instance.PathConfig;
+            }
+            if (!Directory.Exists(Values.Instance.PathConfig + "\\Dailylog"))
+            {
+                Directory.CreateDirectory(Values.Instance.PathConfig + "\\Dailylog");
+            }
+            if (!Directory.Exists(Values.Instance.PathConfig + "\\Statelog"))
+            {
+                Directory.CreateDirectory(Values.Instance.PathConfig + "\\Statelog");
+            }
+            //Create files
+            if(!File.Exists(Values.Instance.PathConfig + "\\Dailylog\\Log.json"))
+            {
+                StreamWriter log = new StreamWriter(Values.Instance.PathConfig + "\\Dailylog\\Log.json");
+                log.WriteLine(Values.Instance.PathTarget);
+                log.Close();
+            }
+            if (!File.Exists(Values.Instance.PathConfig + "\\Dailylog\\Log.xml"))
+            {
+                StreamWriter log = new StreamWriter(Values.Instance.PathConfig + "\\Dailylog\\Log.xml");
+                log.WriteLine(Values.Instance.PathTarget);
+                log.Close();
+            }
+            if (!File.Exists(Values.Instance.PathConfig + "\\Statelog\\Statelog.json"))
+            {
+                StreamWriter state = new StreamWriter(Values.Instance.PathConfig + "\\Statelog\\Statelog.json");
+                state.WriteLine(Values.Instance.PathTarget);
+                state.Close();
+            }
+            if (!File.Exists(Values.Instance.PathConfig + "\\Config\\Path.json"))
+            {
                 StreamWriter path = new StreamWriter(Values.Instance.PathConfig + "\\Config\\Path.json");
                 path.WriteLine(Values.Instance.PathTarget);
                 path.Close();
+            }
+            if (!File.Exists(Values.Instance.PathConfig + "\\Config\\Lang.json"))
+            {
                 Values.Instance.Lang = "en";
                 StreamWriter lang = new StreamWriter(Values.Instance.PathConfig + "\\Config\\Lang.json");
                 lang.WriteLine(Values.Instance.Lang);
                 lang.Close();
+            }
+            if (!File.Exists(Values.Instance.PathConfig + "\\Config\\Ext.json"))
+            {
                 StreamWriter ext = new StreamWriter(Values.Instance.PathConfig + "\\Config\\Ext.json");
                 ext.WriteLine("");
                 ext.Close();
+            }
+            if (!File.Exists(Values.Instance.PathConfig + "\\Config\\File.json"))
+            {
                 Values.Instance.FileExt = ".json";
                 StreamWriter file = new StreamWriter(Values.Instance.PathConfig + "\\Config\\File.json");
                 file.WriteLine(Values.Instance.FileExt);
                 file.Close();
             }
-            else if (Directory.Exists(Values.Instance.PathConfig + "\\Config"))
+            //Reader files
+            if (File.Exists(Values.Instance.PathConfig + "\\Config\\Lang.json"))
             {
                 StreamReader lang = new StreamReader(Values.Instance.PathConfig + "\\Config\\Lang.json");
                 Values.Instance.Lang = lang.ReadLine();
                 lang.Close();
+            }
+            if (File.Exists(Values.Instance.PathConfig + "\\Config\\Path.json"))
+            {
                 StreamReader path = new StreamReader(Values.Instance.PathConfig + "\\Config\\Path.json");
                 Values.Instance.PathTarget = path.ReadLine();
-                lang.Close();
+                path.Close();
+            }
+            if (File.Exists(Values.Instance.PathConfig + "\\Config\\File.json"))
+            {
                 StreamReader file = new StreamReader(Values.Instance.PathConfig + "\\Config\\File.json");
                 Values.Instance.FileExt = file.ReadLine();
                 file.Close();
-            }
-            if (!Directory.Exists(Values.Instance.PathConfig + "\\Dailylog"))
-            {
-                Directory.CreateDirectory(Values.Instance.PathConfig + "\\Dailylog");
-                File.Create(Values.Instance.PathConfig + "\\Dailylog\\Log.json");
-                File.Create(Values.Instance.PathConfig + "\\Dailylog\\Log.xml");
-            }
-            if (!Directory.Exists(Values.Instance.PathConfig + "\\Statelog"))
-            {
-                Directory.CreateDirectory(Values.Instance.PathConfig + "\\Statelog");
-                File.Create(Values.Instance.PathConfig + "\\Statelog\\Statelog.json");
             }
         }
         public void SaveCheck()
